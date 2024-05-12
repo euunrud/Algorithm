15f947cd1404f273b9a8693016196542d8dbef33
eunkyung <88434735+euunrud@users.noreply.github.com>
2024-05-13 00:33:13 +0900

    [Silver II] Title: 스택 수열, Time: 372 ms, Memory: 27788 KB -BaekjoonHub

---
+++ Java/백준/Silver/1874. 스택 수열/스택 수열.java
@@ -0,0 +1,39 @@
+import java.io.*;
+import java.util.ArrayDeque;
+import java.util.LinkedList;
+import java.util.Stack;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+        
+        int num = Integer.parseInt(br.readLine());
+        Stack<Integer> stack =new Stack<>();
+        int last = 0;
+        
+        for(int i=0; i< num; i++){
+            int n = Integer.parseInt(br.readLine());
+            if(n>last){
+                for(int j = last+1 ; j <= n; j++){
+                    stack.push(j);
+                    sb.append("+\n");
+                }
+                last = n;
+            }
+            if(stack.get(stack.size()-1)==n){
+                stack.pop();
+                sb.append("-\n");
+            }
+            }
+        if(stack.size()>0){
+            bw.write("NO");
+        }
+        else {
+            bw.write(sb+"\n");
+        }
+        bw.flush();
+        bw.close();
+        }
+}
