1362288b22d7daf87912e0736614df58526a9267
eunkyung <88434735+euunrud@users.noreply.github.com>
2024-05-19 06:07:41 +0900

    [Gold V] Title: 선 긋기, Time: 2596 ms, Memory: 275884 KB -BaekjoonHub

---
+++ Java/백준/Gold/2170. 선 긋기/선 긋기.java
@@ -0,0 +1,50 @@
+import java.io.*;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.StringTokenizer;
+ 
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+        int N = Integer.parseInt(br.readLine());
+        int[][] arr = new int[N][2];
+        int last; 
+        int sum; 
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            arr[i][0] = Integer.parseInt(st.nextToken());
+            arr[i][1] = Integer.parseInt(st.nextToken());
+        }
+ 
+        Arrays.sort(arr, new Comparator<int[]>() { 
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                if (o1[0] == o2[0])
+                    return o1[1] - o2[1];
+                else
+                    return o1[0] - o2[0];
+            }
+        });
+ 
+        last = arr[0][1]; 
+        sum = arr[0][1] - arr[0][0]; 
+ 
+        for (int i = 1; i < N; i++) {
+            if (arr[i][0] <= last) { 
+                if (arr[i][1] >= last)
+                    sum += arr[i][1] - last; 
+                else
+                    continue;
+            } else {
+                sum += arr[i][1] - arr[i][0]; 
+            }
+            last = arr[i][1]; 
+        }
+        bw.write(String.valueOf(sum)); 
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
