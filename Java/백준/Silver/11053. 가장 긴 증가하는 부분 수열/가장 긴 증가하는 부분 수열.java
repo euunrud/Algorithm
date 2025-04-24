e5f69eff1d604320e1944e6d35b88f5aab6445c1
eunkyung <melon0802@naver.com>
2025-04-24 17:50:11 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 120 ms, Memory: 14508 KB -BaekjoonHub

--- Java/백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
+++ Java/백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
@@ -1,37 +1,45 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] list = new int[n];
+        int[] dp = new int[n];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < n; i++) {
+        	list[i] = Integer.parseInt(st.nextToken());
+        }
 
-		int n = Integer.parseInt(br.readLine());
-		int[] nums = new int[n];
-		int[] dp = new int[n];
-		StringTokenizer sn = new StringTokenizer(br.readLine());
 
-		for (int i = 0; i < n; i++) {
-			nums[i] = Integer.parseInt(sn.nextToken());
-			dp[i] = 1;
-		}
-
-		int maxLeng = 1;
-		for (int i = 1; i < n; i++) {
-			for (int j = 0; j < i; j++) {
-				if (nums[i] > nums[j]) {
-					dp[i] = Math.max(dp[i], dp[j] + 1);
-				}
-			}
-			maxLeng = Math.max(maxLeng, dp[i]);
-		}
-
-		bw.write(maxLeng + "\n");
-		bw.flush();
-		bw.close();
+        int max = 1;
+        dp[0] = 1;
+        if(n > 1) {
+        	dp[1] = list[0] < list[1] ? 2 : 1;
+            if(dp[1] > 1) max = 2;
+        }
+        for(int i = 2; i < n; i++) {
+        	for(int j = i - 1; j >= 0; j--) {
+        		if(list[i] > list[j]) {
+        			if(dp[i] <= dp[j]) {
+        				dp[i] = dp[j] + 1;
+        				//System.out.println(i + " - " + dp[i]);
+        			}
+        		}
+        	}
+        	if(dp[i] == 0) {
+        		dp[i] = 1;
+        	}
+        	//System.out.println(i + " -- " + dp[i]);
+        	if(dp[i] > max) {
+        		max = dp[i];
+        	}
+        }
+        
+        System.out.print(max);
+        
 	}
 }
