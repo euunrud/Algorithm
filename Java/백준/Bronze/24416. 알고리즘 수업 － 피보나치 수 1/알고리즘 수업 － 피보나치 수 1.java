f949f25627e13664fe48d809ce61c5431659f123
eunkyung <melon0802@naver.com>
2025-04-27 22:56:08 +0900

    [Bronze I] Title: 알고리즘 수업 - 피보나치 수 1, Time: 128 ms, Memory: 16044 KB -BaekjoonHub

---
+++ Java/백준/Bronze/24416. 알고리즘 수업 － 피보나치 수 1/알고리즘 수업 － 피보나치 수 1.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		
+		int[] f = new int[n+1];
+		f[1] = f[2] = 1;
+		for(int i = 3; i <= n; i++) {
+			f[i] = f[i - 1] + f[i - 2];
+		}
+		
+		System.out.println(f[n] + " " + (n - 2));
+	}
+}
