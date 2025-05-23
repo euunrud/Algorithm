c70ac3591de1a2131105351f06626c252b01ba19
eunkyung <melon0802@naver.com>
2025-05-23 18:35:27 +0900

    [Gold V] Title: LCS, Time: 156 ms, Memory: 19164 KB -BaekjoonHub

--- Java/백준/Gold/9251. LCS/LCS.java
+++ Java/백준/Gold/9251. LCS/LCS.java
@@ -1,48 +1,52 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.ArrayList;
-import java.util.StringTokenizer;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
-		char[] a = br.readLine().toCharArray();
-		char[] b = br.readLine().toCharArray();
-		int[][] dp = new int[a.length + 1][b.length + 1];
-
-		for (int i = 0; i < a.length; i++) {
-			for(int j = 0; j < b.length; j++) {
-				dp[i][j] = -1;
+		String s1 = br.readLine();
+		String s2 = br.readLine();
+		String[] st1 = s1.split("");
+		String[] st2 = s2.split("");
+		int[][] dp = new int[st2.length][st1.length];
+		int max = 0;
+		for(int i = 0; i < st1.length; i++) {
+			for(int j = 0; j < st2.length; j++) {
+				if(i == 0 && j == 0) {
+					if(st1[i].equals(st2[j])) {
+						dp[0][0] = 1;
+					}else {
+						dp[0][0] = 0;
+					}
+				}
+				if(st1[i].equals(st2[j])) {
+					if(i > 0 && j > 0) {
+						dp[j][i] = dp[j - 1][i - 1] + 1;
+					}else if(i > 0 || j > 0) {
+						dp[j][i] = 1;
+					}
+				}else {
+					if(i > 0 && j > 0) {
+						dp[j][i] = dp[j - 1][i] > dp[j][i - 1]?dp[j - 1][i]:dp[j][i - 1];
+					}else if(i > 0) {
+						dp[j][i] = dp[j][i - 1];
+					}else if(j > 0) {
+						dp[j][i] = dp[j - 1][i];
+					}
+				}
+				//System.out.println("i - " + i+ ", j - " + j + ", " + dp[j][i]);
+				if(max < dp[j][i]) {
+					max = dp[j][i];
+				}
 			}
 		}
-		int rslt = LCS(a, b, dp, a.length - 1, b.length - 1);
-		bw.write(rslt + "\n");
-		bw.flush();
-		bw.close();
-	}
-
-	public static int LCS(char[] a, char[] b, int[][] dp, int al, int bl) {
-		if(al == -1 || bl == -1) {
-			return 0;
-		}
-
-		if(dp[al][bl] == -1) {
-			dp[al][bl] = 0;
-			//같을 때 -> 값을 +1 해서 재귀 값 저장
-			if(a[al] == b[bl]) {
-				dp[al][bl] = LCS(a, b, dp, al - 1, bl - 1) + 1;
-			}
-
-			// 같지 X-> LCS(dp)[al-1][bl]와 LCS(dp)[al, bl-1] 중 큰 값
-			else {
-				dp[al][bl] = Math.max(LCS(a, b, dp, al - 1, bl), LCS(a, b, dp, al, bl - 1));
-			}
-		}
-		return dp[al][bl];
+//		for(int i = 0; i < st1.length; i++) {
+//			for(int j = 0; j < st2.length; j++) {
+//				System.out.print(dp[j][i]);
+//			}
+//			System.out.println();
+//		}
+		System.out.println(max);
 	}
 }
