e675115b9bb6493b96d866841019574026b21b4e
eunkyung <melon0802@naver.com>
2025-10-02 21:27:29 +0900

    [Gold III] Title: 나머지 합, Time: 444 ms, Memory: 120628 KB -BaekjoonHub

--- 백준/Gold/10986. 나머지 합/나머지 합.java
+++ 백준/Gold/10986. 나머지 합/나머지 합.java
@@ -1,35 +1,31 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
-public class Main {
-
-	public static void main(String[] args) throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int n = Integer.parseInt(st.nextToken());
-		int m = Integer.parseInt(st.nextToken());
-		
-		long[] S = new long[n + 1];
-		long[] cnt = new long[m];
-		st = new StringTokenizer(br.readLine());
-		for(int i = 1; i < n + 1; i++) {
-			if(i == 1) {
-				S[i] = Long.parseLong(st.nextToken());
-			}else {
-				S[i] = S[i - 1] + Long.parseLong(st.nextToken());
-			}
-		}
-		
-		for(int i = 1; i < n + 1; i++) {
-			int num = (int)(S[i] % m);
-			cnt[num]++;
-		}
-		Long rslt = 0L;
-		rslt += cnt[0];
-		for(int i = 0; i < m; i++) {
-			Long num = cnt[i];
-			rslt += num * (num - 1) / 2;
-		}
-		System.out.println(rslt);
-	}
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        long[] nums = new long[n + 1];
+        long[] cnt = new long[m];
+        long sum = 0L;
+       
+        st = new StringTokenizer(br.readLine());
+        for (int i = 1; i <= n; i++) {
+            nums[i] = nums[i - 1] + Long.parseLong(st.nextToken());
+        }
+        
+        for (int i = 1; i <= n; i++) {
+            int num = (int)(nums[i] % m);
+            cnt[num]++;
+        }
+        
+        for(int c = 0; c < m; c++) {
+            sum += cnt[c] * (cnt[c] - 1) / 2;
+        }
+        
+        sum += cnt[0];
+        System.out.println(sum);
+    }
 }
