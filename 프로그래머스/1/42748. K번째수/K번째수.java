de3dce46167db7ebd902d1528dad948b4dfa70df
eunkyung <melon0802@naver.com>
2025-10-15 11:59:17 +0900

    [level 1] Title: K번째수, Time: 2.90 ms, Memory: 75 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.java
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -3,18 +3,11 @@ class Solution {
     public int[] solution(int[] array, int[][] commands) {
         int[] answer = new int[commands.length];
 
-        for(int i =0; i < commands.length; i++) {
-            int leng = commands[i][1] - commands[i][0] + 1;
-            int[] arr = new int[leng];
-            int k = 0;
-            for(int j = commands[i][0] - 1; j < commands[i][1]; j++) {
-                arr[k++] = array[j];
-            }
+        for(int i = 0; i < commands.length; i++) {
+            int[] arr = Arrays.stream(array, commands[i][0] - 1, commands[i][1]).toArray();
             Arrays.sort(arr);
-            answer[i] = arr[commands[i][2]-1];
+            answer[i] = arr[commands[i][2] - 1];
         }
-
-        //System.out.println(Arrays.toString(answer));
         return answer;
     }
 }
