389fb5a070035313ca5718c33e144270ec508456
eunkyung <melon0802@naver.com>
2025-11-17 22:33:01 +0900

    [level 3] Title: 입국심사, Time: 100.43 ms, Memory: 89.3 MB -BaekjoonHub

--- 프로그래머스/3/43238. 입국심사/입국심사.java
+++ 프로그래머스/3/43238. 입국심사/입국심사.java
@@ -2,29 +2,24 @@ import java.util.*;
 
 class Solution {
     public long solution(int n, int[] times) {
-        long answer = 0;
         Arrays.sort(times);
-        int len = times.length;
-        
         long left = 1;
-        long right = (long)times[len - 1] * n;
-        answer = right;
+        long right = (long)times[times.length - 1] * n;
+        long answer = right;
         
         while(left <= right) {
             long mid = (left + right) / 2;
-            long cnt = 0;
-            for(int i = 0; i < times.length; i++) {
-                cnt += mid / times[i];
-            }
+            long num = 0;
+            for(int i = 0; i < times.length; i++)
+                num += mid / times[i];
             
-            if(cnt >= n) {
+            if(num >= n){
                 answer = mid;
                 right = mid - 1;
-            }else {
-                left = mid + 1;
             }
+            else
+                left = mid + 1;
         }
-        
         return answer;
     }
 }
