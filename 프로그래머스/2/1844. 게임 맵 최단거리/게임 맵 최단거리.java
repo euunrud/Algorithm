cae2c1fbcd82c9b31ca1330e86ef1c8479d240c1
eunkyung <melon0802@naver.com>
2025-11-24 21:01:59 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 9.60 ms, Memory: 54.8 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -2,35 +2,34 @@ import java.util.*;
 
 class Solution {
     public int solution(int[][] maps) {
-        int answer = 0;
+        int answer = -1;
+        Queue<int[]> que = new ArrayDeque<>();
+        que.offer(new int[]{0, 0, 1});
+        maps[0][0] = 0;
+        
         int n = maps.length;
         int m = maps[0].length;
-        boolean[][] vst = new boolean[n][m];
-        Queue<int[]> que = new ArrayDeque<>();
         
-        que.offer(new int[]{0, 0, 1});
-        vst[0][0] = true;
+        int[] dx = {-1, 1, 0, 0};
+        int[] dy = {0, 0, -1, 1};
         while(!que.isEmpty()) {
-            int[] a = que.poll();
+            int[] arr = que.poll();
             
-            if(a[0] == n - 1 && a[1] == m - 1) {
-                answer = a[2];
+            if(arr[0] == n - 1 && arr[1] == m - 1){
+                answer = arr[2];
                 break;
             }
-            int[] dx = {0,0,1,-1};
-            int[] dy = {1,-1,0,0};
             for(int i = 0; i < 4; i++) {
-                int nx = a[0] + dx[i];
-                int ny = a[1] + dy[i];
-                if(nx >= n || nx < 0 || ny >= m || ny < 0) 
-                    continue;
-                if(maps[nx][ny] == 0 || vst[nx][ny] == true) 
-                    continue;
-                vst[nx][ny] = true;
-                que.offer(new int[]{nx, ny, a[2] + 1});
+                if(arr[0] + dx[i] >= 0 && arr[0] + dx[i] < n && arr[1] + dy[i] >= 0 && arr[1] + dy[i] < m) {
+                    if(maps[arr[0] + dx[i]][arr[1] + dy[i]] == 1) {
+                        que.offer(new int[]{arr[0] + dx[i], arr[1] + dy[i], arr[2] + 1});
+                        maps[arr[0] + dx[i]][arr[1] + dy[i]] = 0;
+                    }
+                }
+                            
             }
         }
-        answer = answer != 0 ? answer : -1;
+        
         return answer;
     }
 }
