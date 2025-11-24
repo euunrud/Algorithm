bc3b2542a3253b7ee4d94b2d8c43a6764b5addae
eunkyung <melon0802@naver.com>
2025-11-24 16:09:07 +0900

    [level 2] Title: 큰 수 만들기, Time: 7079.02 ms, Memory: 100 MB -BaekjoonHub

--- 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.java
+++ 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.java
@@ -1,29 +1,38 @@
-import java.util.*;
-
 class Solution {
     public String solution(String number, int k) {
         String answer = "";
-        char[] cArr = number.toCharArray();
-        Deque<Character> stack = new ArrayDeque<>();
-        
-        stack.addLast(cArr[0]);
-        for(int i = 1; i < cArr.length; i++) {
-            while(!stack.isEmpty() && k > 0 && stack.peekLast() < cArr[i]) {
-                stack.pollLast();
-                k--;
-            }
-            stack.addLast(cArr[i]);
-        }
-    
-        while(k > 0) {
-            stack.pollLast();
-            k--;
+        int[] arr = new int[number.length()];
+        int a = 0;
+        for(char c : number.toCharArray()) {
+            arr[a++] = c - '0';
         }
         
-       StringBuilder sb = new StringBuilder();
-        while(!stack.isEmpty()) {
-            sb.append(stack.pollFirst());
+        int idx = 0;
+        int cnt = k;
+        StringBuilder sb = new StringBuilder();
+
+        while (idx < arr.length && cnt > 0) {
+            int end = Math.min(arr.length - 1, idx + cnt);
+            int max = arr[idx];
+            int mIdx = idx;
+
+            for (int i = idx; i <= end; i++)
+                if (arr[i] > max) {
+                    max = arr[i];
+                    mIdx = i;
+                }
+
+            sb.append(max);
+            cnt -= (mIdx - idx);
+            idx = mIdx + 1;
         }
+        
+        for (int i = idx; i < arr.length; i++)
+            sb.append(arr[i]);
+
+        if (cnt > 0)
+            sb.setLength(sb.length() - cnt);
+        
         answer = sb.toString();
         return answer;
     }
