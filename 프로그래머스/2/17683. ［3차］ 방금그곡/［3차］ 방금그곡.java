07fe94796a769e32cae77a0d5146f5d5c7b156ad
eunkyung <melon0802@naver.com>
2026-02-18 00:46:03 +0900

    [level 2] Title: [3차] 방금그곡, Time: 49.25 ms, Memory: 113 MB -BaekjoonHub

---
+++ 프로그래머스/2/17683. ［3차］ 방금그곡/［3차］ 방금그곡.java
@@ -0,0 +1,68 @@
+class Solution {
+    static String change(String s) {
+        for(int i = 0; i < s.length(); i++){
+            if(s.charAt(i) == '#') {
+                char c = s.charAt(i - 1);
+                c += 32;
+                s = s.substring(0, i - 1) + c + s.substring(i + 1, s.length());
+                i--;
+            }
+        }
+        return s;
+    }
+    static int calc(String s1, String s2) {
+        int time = 0;
+        int hh1 = Integer.parseInt(s1.split(":")[0]);
+        int mm1 = Integer.parseInt(s1.split(":")[1]);
+        int hh2 = Integer.parseInt(s2.split(":")[0]);
+        int mm2 = Integer.parseInt(s2.split(":")[1]);
+        
+        if(hh1 == hh2)
+            time = mm2 - mm1;
+        else
+            time = (hh2 - hh1 - 1) * 60 + (60 - mm1) + mm2;
+        
+        return time;
+    }
+    public String solution(String m, String[] musicinfos) {
+        String answer = "";
+        int len = musicinfos.length;
+        String[][] infos = new String[len][4];
+        
+        m = change(m);
+        for(int i = 0; i < len; i++) {
+            infos[i] = musicinfos[i].split(",");
+            int play = calc(infos[i][0], infos[i][1]);
+            String ns = "";
+            
+            for(int j = 2; j < 4; j++)
+                if(infos[i][j].contains("#"))
+                    infos[i][j] = change(infos[i][j]);
+            
+            while(play > 0) {
+                if(play >= infos[i][3].length()) {
+                    ns += infos[i][3];
+                    play -= infos[i][3].length();
+                } else {
+                    ns += infos[i][3].substring(0, play);
+                    play = 0;
+                }
+            }
+            infos[i][3] = ns;
+        }
+        
+        String rslt = "";
+        int pTime = -1;
+        for(int i = 0; i < len; i++)
+            if(infos[i][3].contains(m)) {
+                int t = calc(infos[i][0], infos[i][1]);
+                if(t > pTime) {
+                    rslt = infos[i][2];
+                    pTime = t;
+                }
+            }
+        
+        if(pTime == -1) rslt = "(None)";
+        return rslt;
+    }
+}
