fa23e1a8f4a4c81761708564492e6b1cc60440da
eunkyung <melon0802@naver.com>
2026-03-06 22:31:42 +0900

    [level 3] Title: 등굣길, Time: 0.66 ms, Memory: 54.1 MB -BaekjoonHub

--- 프로그래머스/3/42898. 등굣길/README.md
+++ 프로그래머스/3/42898. 등굣길/README.md
@@ -1,10 +1,10 @@
 # [level 3] 등굣길 - 42898 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42898) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42898#) 
 
 ### 성능 요약
 
-메모리: 54 MB, 시간: 0.60 ms
+메모리: 54.1 MB, 시간: 0.66 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 11월 03일 16:03:48
+2026년 03월 06일 22:31:40
 
 ### 문제 설명
 
--- 프로그래머스/3/42898. 등굣길/등굣길.java
+++ 프로그래머스/3/42898. 등굣길/등굣길.java
@@ -1,24 +1,20 @@
+import java.util.*;
+
 class Solution {
     public int solution(int m, int n, int[][] puddles) {
-        int answer = 0;
+        boolean[][] puddle = new boolean[n + 1][m + 1];
+        for (int[] p : puddles)
+            puddle[p[1]][p[0]] = true;
+        
         int[][] dp = new int[n + 1][m + 1];
+        dp[1][1] = 1;
         
-        for(int[] p : puddles) {
-            dp[p[1]][p[0]] = -1;
-        }
-        
-        dp[0][1] = 1;
-        for(int i = 1; i <= m; i++) { 
-            for(int j = 1; j <= n; j++) {
-                if(dp[j][i] == -1) 
-                    dp[j][i] = 0;
-                else 
-                    dp[j][i] = (dp[j - 1][i] + dp[j][i - 1]) % 1000000007;
+        for(int i = 1; i <= n; i++)
+            for(int j = 1; j <= m; j++) {
+                if(puddle[i][j] == true || i == 1 && j == 1)
+                    continue;
+                dp[i][j] = (dp[i - 1][j] + dp[i][j - 1]) % 1000000007;
             }
-        }
-        
-        answer = dp[n][m] % 1000000007;
-        return answer;
+        return dp[n][m] % 1000000007;
     }
-    
 }
