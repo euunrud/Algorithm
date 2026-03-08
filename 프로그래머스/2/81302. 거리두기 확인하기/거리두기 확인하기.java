e9562f71386f8fadf59a490a22557ea33b6b3ba8
eunkyung <melon0802@naver.com>
2026-03-08 15:04:40 +0900

    [level 2] Title: 거리두기 확인하기, Time: 0.14 ms, Memory: 99.1 MB -BaekjoonHub

--- 프로그래머스/2/81302. 거리두기 확인하기/거리두기 확인하기.java
+++ 프로그래머스/2/81302. 거리두기 확인하기/거리두기 확인하기.java
@@ -1,67 +1,55 @@
-class Solution {
-    public int check(String[] s, int j1, int k1) {
-        int j = j1;
-        int k = k1;
+import java.util.*;
 
-        j++;        
-        while(j < 5 && j <= j1 + 2) {
-            if(s[j].charAt(k) =='P') return 0;
-            else if(s[j].charAt(k) == 'X') break; 
-            j++;
-        }
-        
-        j = j1;
-        k++;
-        while(k < 5 && k <= k1 + 2) {
-            if(s[j].charAt(k) == 'P') return 0;
-            else if(s[j].charAt(k) == 'X') break; 
-            k++;
-        }
-        
-        if(k1 < 4 && j1 < 4) {
-            if(s[j1 + 1].charAt(k1 + 1) == 'P') {
-                if(s[j1 + 1].charAt(k1) == 'O' || s[j1].charAt(k1 + 1) == 'O') return 0;
-            }
-        }
-        
-        if(k1 > 0 && j1 < 4) {
-            if(s[j1 + 1].charAt(k1 - 1) == 'P') {
-                if(s[j1 + 1].charAt(k1) == 'O' || s[j1].charAt(k1 - 1) == 'O') return 0;
-            }
-        }
-        
-        return 1;
-    }
-    
+class Solution {
     public int[] solution(String[][] places) {
-        int leng = places[0].length;
-        
-        int[] answer = new int[leng];
-        for(int i = 0; i < leng; i++) {
-            String[] s = places[i];
-            if(answer[i] == 0) { 
-                for(int j = 0; j < s.length; j++) {
-                    if(answer[i] == 0) {
-                        for(int k = 0; k < 5; k++) {
-                            if(s[j].charAt(k) == 'P'){
-                                if(check(s, j, k) == 0){
-                                    answer[i] = -1;
-                                    break;
+        int[] answer = new int[5];
+        Arrays.fill(answer, 1);
+        Set<int[]> set = new HashSet<>();
+        for(int i = 0; i < 5; i++) {
+            for(int j = 0; j < 5; j++) {
+                if(answer[i] == 0) break;
+                for(int k = 0; k < 5; k++) {
+                    if(answer[i] == 0) break;
+                    //p이면자신의 상하좌우에서  'O'인 것 중에서 거리 2 이하인거 있나 검사 
+                    if(places[i][j].charAt(k) == 'P') {
+                        int[] dx = {0, 0, -1, 1};
+                        int[] dy = {-1, 1, 0, 0};
+                        Queue<int[]> que = new ArrayDeque<>();
+                        boolean[][] vst = new boolean[5][5];
+                        vst[j][k] = true;
+                        for(int a = 0; a < 4; a++) {
+                            int nx = j + dx[a];
+                            int ny = k + dy[a];
+                            if(nx >= 0 && nx < 5 && ny >= 0 && ny < 5 && vst[nx][ny] == false && places[i][nx].charAt(ny) != 'X') {
+                                vst[nx][ny] = true;
+                                que.offer(new int[]{nx, ny, 1});
+                            }
+                        }
+                        
+                        while(!que.isEmpty()) {
+                            int[] q= que.poll();
+                            if(places[i][q[0]].charAt(q[1]) == 'P') {
+                                answer[i] = 0;
+                                break;
+                            }
+                            
+                            if(q[2] == 1) {
+                                for(int a = 0; a < 4; a++) {
+                                    int nx = q[0] + dx[a];
+                                    int ny = q[1] + dy[a];
+                                    if(nx >= 0 && nx < 5 && ny >= 0 && ny < 5 && vst[nx][ny] == false && places[i][nx].charAt(ny) != 'X') {
+                                        vst[nx][ny] = true;
+                                        que.offer(new int[]{nx, ny, q[2] + 1});
+                                    }
                                 }
                             }
                         }
+                        
                     }
                 }
             }
-            if(answer[i] == 0) {
-                answer[i] = 1;
-            }else {
-                answer[i] = 0;
-            }
+            
         }
-        //p이면 자신의 아래(2칸까지), 오른쪽(2칸까지), 대각선에에 p없는지, 있더라고 그 사이에 파티션이 있는지 CHECK
-        //있으면 바로 0, 위나 왼쪽은 이미 체크되어서 내려왔기에 굳이 체크 안해도 될듯 ㅎㅅㅎ
-        
         return answer;
     }
 }
