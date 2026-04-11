b19ab53f6d8d3254692fd0d7b074804d827b1bf5
eunkyung <melon0802@naver.com>
2026-04-11 17:09:03 +0900

    [Gold IV] Title: 가장 긴 증가하는 부분 수열 4, Time: 164 ms, Memory: 17672 KB -BaekjoonHub

--- 백준/Gold/14002. 가장 긴 증가하는 부분 수열 4/가장 긴 증가하는 부분 수열 4.java
+++ 백준/Gold/14002. 가장 긴 증가하는 부분 수열 4/가장 긴 증가하는 부분 수열 4.java
@@ -1,59 +1,58 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 class Main {
-    static int[] memo, nums;
     static int n;
-    static String[] words;
+    static int[] arr;
+    static int[] memo;
+    static String[] srr;
+    
     static int dp(int idx) {
-      if(idx == n)
-        return 1;
-
-      if(memo[idx] != 0)
+        if(memo[idx] != 0) 
+            return memo[idx];
+        
+        int max = 0;
+        for(int i = idx + 1; i < n; i++) {
+            if(arr[i] > arr[idx]) {
+                int a = dp(i);
+                
+                if(a > max) {
+                    max = a;
+                    srr[idx] = arr[idx] + " " + srr[i];
+                }
+                
+            }
+        }
+        
+        if(srr[idx] == null) srr[idx] = arr[idx] + "";
+        memo[idx] = max + 1;
+        
         return memo[idx];
-      
-      int max = 0;
-      String rs = "";
-      for(int i = idx + 1; i <= n; i++) 
-        if(nums[i] > nums[idx]) {
-          int len = dp(i);
-          String str = words[i];
-          
-          if(len > max) {
-            max = len;
-            rs = str;
-          }
-        }
-
-      memo[idx] = max + 1;
-      words[idx] = String.valueOf(nums[idx]).concat(" " + rs);
-      //System.out.println("idx" + idx + " memo " + memo[idx] + " " + words[idx]);
-      return memo[idx];
     }
-  
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
         n = Integer.parseInt(br.readLine());
-      
         StringTokenizer st = new StringTokenizer(br.readLine());
-        nums = new int[n + 1];
-        for(int i = 1; i <= n; i++)
-          nums[i] = Integer.parseInt(st.nextToken());
-
-        memo = new int[n + 1];
-        words = new String[n  +1];
-        words[n] = String.valueOf(nums[n]);
-        int max = 1;
-        String rs = String.valueOf(nums[1]);
-        for(int i = 1; i <= n - max; i++) {
-          int rslt = dp(i);
-          if(rslt > max) {
-            max = rslt;
-            rs = words[i] ;
-          }
+        arr = new int[n];
+        memo = new int[n];
+        srr = new String[n];
+        
+        for(int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
         }
-      
+        
+        int max = 0;
+        int maxIdx = 0;
+        for(int i = 0; i < n; i++) {
+            int rslt = dp(i);
+            if(rslt > max) {
+                max = rslt;
+                maxIdx = i;
+            }
+        }
+        
         System.out.println(max);
-        System.out.println(rs);
-  }
+        System.out.print(srr[maxIdx]);
+    }
 }
